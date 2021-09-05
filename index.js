e660669516dd91d2c688134f88fd556985dc7b3c
skmetheloper <ethereal97@gmail.com>
2021-09-05 21:08:15 +0630

    a

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+const {
+    FACEBOOK_MESSENGING_TOKEN,
+    APP_SECRET
+} = process.env;
 const PORT = process.env.PORT || 3000;
+const { default: axios } = require('axios');
 const e = require('express');
 const messages = [];
 
@@ -10,7 +15,6 @@ const app = e();
 app.use(e.json());
 
 app.get('/webhook', (req, res) => {
-    const APP_SECRET = process.env.APP_SECRET;
     const token = req.query['hub.verify_token'];
     const challenege = req.query['hub.challenge'];
     if (token !== APP_SECRET) {
@@ -20,13 +24,40 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-    let body = req.body;
-    messages.push(body);
+    let { object, entry } = req.body;
+    if (object !== 'page') return res.status(400).json({ error: 'bad request' });
     res.status(200).send('OK');
+
+    entry.forEach(({ sender, message }) => {
+        let recipient = {
+            id: sender.id
+        };
+        let is_greeted = messages.filter(m => {
+            return m.sender.id === recipient.id;
+        }).length;
+        if (is_greeted) {
+            return;
+        }
+        messages.push(e);
+        axios.post(`https://graph.facebook.com/me/messages?access_token=${FACEBOOK_MESSENGING_TOKEN}`, {
+                recipient,
+                message: { text: 'မင်္ဂလာပါ' },
+                messaging_type: "RESPONSE"
+            })
+            .catch(err => console.error(err, err.message));
+        setTimeout(() => {
+            axios.post(`https://graph.facebook.com/me/messages?access_token=${FACEBOOK_MESSENGING_TOKEN}`, {
+                    recipient,
+                    message: { text: 'Ezreal Creation မှ ကြိုဆိုပါတယ်။' },
+                    messaging_type: "RESPONSE"
+                })
+                .catch(err => console.error(err, err.message));
+        }, 800);
+    });
 });
 
 app.get('/messages', (req, res) => {
-    if (req.query.token !== process.env.APP_SECRET) return res.status(403).json({ error: 'Forbidden' });
+    if (req.query.token !== APP_SECRET) return res.status(403).json({ error: 'Forbidden' });
     res.json({ data: messages, });
 });
 
